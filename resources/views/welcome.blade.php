0d507147fefd35c922002edac515eb973242aba7
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-11-09 01:59:47 +0700

    enhance welcome page styling with background color and padding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body class="bg-gray-900 text-white">
     <div class="flex flex-col items-center justify-center min-h-screen">
-        <h1 class="text-3xl font-bold">
+        <h1 class="text-3xl bg-blue-500 p-4 rounded-xl">
             Welcome to Laravel Basic
         </h1>
     </div>
