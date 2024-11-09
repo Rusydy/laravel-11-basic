fe47537e0da2da0445ad6407721801ed6cf1f51c
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-11-09 16:01:27 +0700

    add about page and link from welcome page

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,25 +1,24 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Basic</title>
     @vite([
         'resources/css/app.css',
     ])
+    <title>About Page</title>
 </head>
-<body class="bg-gray-900 text-white">
+<body class="bg-gray-800 text-white">
     <div class="flex flex-col items-center justify-center min-h-screen space-y-4">
-        <a 
-        href="{{ route('about.index') }}" 
+        <a
+        href="{{ route('welcome') }}"
         class="bg-blue-500 text-white px-4 py-2 rounded-lg"
         >
-            About
+            Home
         </a>
+
         <h1 class="text-3xl bg-blue-500 p-4 rounded-xl">
-            Welcome to Laravel Basic
+            About Page
         </h1>
     </div>
-
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/about', function () {
+    return view('about.index');
+})->name('about.index');
